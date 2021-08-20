1549d920b463dc4ae8db1e9c28306942e5e21b41
ATE47 <wilate@yahoo.fr>
2021-08-20 22:32:02 +0200

    switch to act command injection

--- src/main/java/fr/atesab/specttp/SpectTpMain.java
+++ src/main/java/fr/atesab/specttp/SpectTpMain.java
@@ -1,17 +1,37 @@
 package fr.atesab.specttp;
 
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import com.google.common.collect.Maps;
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.ParseResults;
+import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.builder.ArgumentBuilder;
+import com.mojang.brigadier.builder.RequiredArgumentBuilder;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.tree.CommandNode;
+import com.mojang.brigadier.tree.RootCommandNode;
+
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.PlayerInfo;
-import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+import net.minecraft.commands.SharedSuggestionProvider;
+import net.minecraft.commands.synchronization.SuggestionProviders;
 import net.minecraft.network.chat.ClickEvent;
+import net.minecraft.network.chat.ComponentUtils;
+import net.minecraft.network.chat.HoverEvent;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.network.protocol.game.ServerboundTeleportToEntityPacket;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.GameType;
+import net.minecraftforge.client.event.ClientChatEvent;
+import net.minecraftforge.client.event.GuiScreenEvent.InitGuiEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -24,9 +44,124 @@ public class SpectTpMain {
     public static final String MOD_AUTHOR = "ATE47";
     public static final String COMMAND = "sptp";
 
+    private static CommandDispatcher<CommandSourceStack> dispatcher = new CommandDispatcher<>();
+    private static CommandDispatcher<SharedSuggestionProvider> SharedSuggestionProvider;
+    private static Set<String> commandSet = new HashSet<>();
+
     public SpectTpMain() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    // ACT command System
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    private void createSuggestion(CommandNode<CommandSourceStack> dispatcher,
+            CommandNode<SharedSuggestionProvider> rootCommandNode, CommandSourceStack player,
+            Map<CommandNode<CommandSourceStack>, CommandNode<SharedSuggestionProvider>> suggestions) {
+        for (CommandNode<CommandSourceStack> child : dispatcher.getChildren()) {
+            ArgumentBuilder<SharedSuggestionProvider, ?> argumentbuilder = (ArgumentBuilder) child.createBuilder();
+            argumentbuilder.requires((ctx) -> true);
+            if (argumentbuilder.getCommand() != null)
+                argumentbuilder.executes((ctx) -> 0);
+
+            if (argumentbuilder instanceof RequiredArgumentBuilder) {
+                RequiredArgumentBuilder<SharedSuggestionProvider, ?> requiredargumentbuilder = (RequiredArgumentBuilder) argumentbuilder;
+                if (requiredargumentbuilder.getSuggestionsProvider() != null) {
+                    requiredargumentbuilder
+                            .suggests(SuggestionProviders.safelySwap(requiredargumentbuilder.getSuggestionsProvider()));
+                }
+            }
+
+            if (argumentbuilder.getRedirect() != null) {
+                argumentbuilder.redirect(suggestions.get(argumentbuilder.getRedirect()));
+            }
+
+            CommandNode<SharedSuggestionProvider> commandnode1 = argumentbuilder.build();
+            suggestions.put(child, commandnode1);
+            rootCommandNode.addChild(commandnode1);
+            if (!child.getChildren().isEmpty()) {
+                this.createSuggestion(child, commandnode1, player, suggestions);
+            }
+        }
+
+    }
+
+    private void injectSuggestions() {
+        Minecraft mc = Minecraft.getInstance();
+        if (mc.player != null && mc.player.connection != null) {
+            CommandDispatcher<SharedSuggestionProvider> current = mc.player.connection.getCommands();
+            if (current != SharedSuggestionProvider) {
+                SharedSuggestionProvider = current;
+                if (current != null) {
+                    Map<CommandNode<CommandSourceStack>, CommandNode<SharedSuggestionProvider>> map = Maps.newHashMap();
+                    RootCommandNode<SharedSuggestionProvider> root = SharedSuggestionProvider.getRoot();
+                    map.put(dispatcher.getRoot(), root);
+                    createSuggestion(dispatcher.getRoot(), root, mc.player.createCommandSourceStack(), map);
+                }
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public void onInitGui(InitGuiEvent.Post ev) {
+        injectSuggestions();
+    }
+
+    @SubscribeEvent
+    public void onChatMessage(ClientChatEvent ev) {
+        String msg = ev.getMessage();
+        // check if it is a command
+        if (!msg.startsWith("/"))
+            return;
+
+        final String command = msg.substring(1);
+
+        // check if we know it
+        if (!commandSet.contains(command.split(" ", 2)[0].toLowerCase()))
+            return;
+
+        // we know it, we remove it
+        ev.setCanceled(true);
+        Minecraft.getInstance().gui.getChat().addRecentChat(msg);
+
+        StringReader reader = new StringReader(msg);
+        if (reader.canRead())
+            reader.skip(); // remove the '/'
+        CommandSourceStack source = Minecraft.getInstance().player.createCommandSourceStack();
+
+        try {
+            ParseResults<CommandSourceStack> parse = dispatcher.parse(reader, source);
+            dispatcher.execute(parse);
+        } catch (CommandSyntaxException e) {
+            source.sendFailure(ComponentUtils.fromMessage(e.getRawMessage()));
+            if (e.getInput() != null && e.getCursor() >= 0) {
+                int messageSize = Math.min(e.getInput().length(), e.getCursor());
+                MutableComponent error = (new TextComponent("")).withStyle(ChatFormatting.GRAY)
+                        .withStyle(s -> s.withClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, msg)));
+                if (messageSize > 10) {
+                    error.append("...");
+                }
+
+                error.append(e.getInput().substring(Math.max(0, messageSize - 10), messageSize));
+                if (messageSize < e.getInput().length()) {
+                    MutableComponent Component2 = (new TextComponent(e.getInput().substring(messageSize)))
+                            .withStyle(new ChatFormatting[] { ChatFormatting.RED, ChatFormatting.UNDERLINE });
+                    error.append(Component2);
+                }
+
+                error.append((new TranslatableComponent("command.context.here"))
+                        .withStyle(new ChatFormatting[] { ChatFormatting.RED, ChatFormatting.ITALIC }));
+                source.sendFailure(error);
+            }
+        } catch (Exception e) {
+            MutableComponent error = new TextComponent(
+                    e.getMessage() == null ? e.getClass().getName() : e.getMessage());
+            source.sendFailure((new TranslatableComponent("command.failed")).withStyle((style) -> {
+                style.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, error));
+                return style;
+            }));
+        }
     }
 
     /**
@@ -53,11 +188,8 @@ public class SpectTpMain {
     }
 
     private void setup(final FMLCommonSetupEvent event) {
-    }
-
-    @SubscribeEvent
-    public void registerCommand(RegisterCommandsEvent ev) {
-        ev.getDispatcher().register(Commands.literal("sptp")
+        commandSet.add(COMMAND);
+        dispatcher.register(Commands.literal(COMMAND)
                 .then(Commands.argument("username", ConnectionPlayerArgument.player()).executes(c -> {
                     var to = ConnectionPlayerArgument.getPlayer(c, "username");
                     c.getSource().sendSuccess(new TranslatableComponent("specttp.tp", to.getProfile().getName()),
@@ -72,4 +204,5 @@ public class SpectTpMain {
                     return 1;
                 }));
     }
+
 }
